cea2d89b1026c2425bcb81547e0d81e9dfadc06d
Gerardo Campero <geragrungeeb@gmail.com>
2023-08-31 23:53:47 -0300

    Se genera el archivo gamesHandlers en la carpeta handler y se enrutan los handlers básicos de games

---
+++ Back/src/routes/gamesRout.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+
+const { getGames, getGameById , postGame} = require =('../handlers/gamesHandlers');
+
+const gamesHandler = Router();
+
+gamesHandler.get('/', getGames);
+
+gamesHandler.get('/:name', getGameById);
+
+gamesHandler.post('/', postGame);
+
+
+module.exports = gamesHandler;
