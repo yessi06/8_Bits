c892174416f7989ae337f494957eb1c099a371b8
yessi06 <yessicac187@gmail.com>
2023-09-04 18:04:16 -0500

    Logica y Ruta de Post

--- Back/src/routes/gamesRout.js
+++ Back/src/routes/gamesRout.js
@@ -3,6 +3,8 @@ const { Router } = require('express');
 const { getGameByNameHandler, getGameByIdHandler, filterGameHandler } = require('../handlers/gameHandlers');
 
 const { createGame } = require('../controllers/saveGames/saveGames');
+const { postGame } = require('../controllers/postGame');
+
 
 const gamesRouter = Router();
 
@@ -13,6 +15,12 @@ gamesRouter.post('/saveGames', createGame);
 gamesRouter.get('/', getGameByNameHandler);
 gamesRouter.get('/:id', getGameByIdHandler);
 gamesRouter.post('/filter', filterGameHandler);
+gamesRouter.post('/postGame', postGame);
+
+
+
+
+
 
 
 module.exports = gamesRouter;
