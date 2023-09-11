fb9b924f305928aa8a1c841744bd93043c80d5c8
TwilightMaster23 <briansilva1705@gmail.com>
2023-09-10 22:55:13 -0300

    Se configura passport para la autenticacion local de login

---
+++ Back/src/passportConfig.js
@@ -0,0 +1,45 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const { User } = require('./db');
+
+passport.use(
+    new LocalStrategy({
+        usernameField: 'email', 
+        passwordField: 'password', 
+    }, async (email, password, done) => {
+        try {
+            console.log('Email:', email);
+            console.log('Password:', password);
+
+            const user = await User.findOne({where: { email } });
+            if(!user) {
+                return done(null, false, { message: 'User not found'})
+            }
+
+            const validPassword = await bcrypt.compare(password, user.password)
+            if (validPassword) {
+                return done(null, user)
+            } else {
+                return done(null, false, {message: 'Incorrect password'})
+            }
+        } catch (error) {
+            return done(error)
+        }
+    })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findByPk(id);
+        done(null, user);
+    } catch (error) {
+        done(error);
+    }
+});
+
+module.exports = passport;
