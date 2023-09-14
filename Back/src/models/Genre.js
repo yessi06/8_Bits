826a9c2f3b0c23b78f1e82d1f9448ce0d388edb4
SteveFaj <stevefajardo@gmail.com>
2023-09-14 18:30:00 +0100

    modularizacion y correccion de gender a genre

---
+++ Back/src/models/Genre.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('Genre', {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            //type: DataTypes.ARRAY(DataTypes.STRING),
+            allowNull: false,
+            unique: true,
+        },
+      }, { timestamps: false })
+};
