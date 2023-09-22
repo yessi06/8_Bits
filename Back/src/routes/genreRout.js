ce040c735328b0128b588654cb03ace2bfb2598f
TwilightMaster23 <briansilva1705@gmail.com>
2023-09-22 19:07:32 -0300

    Se agrega la ruta para traer estadisticas

--- Back/src/routes/genreRout.js
+++ Back/src/routes/genreRout.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
-const { getGenre } = require('../handlers/genreHandlers');
+const { getGenre, genreStatistics } = require('../handlers/genreHandlers');
 const {loadGenres} = require('../controllers/saveGamesControllers/saveGames');
 const genreRouter = Router();
 
 genreRouter.get('/', getGenre);
+genreRouter.get('/statistics', genreStatistics);
 
 genreRouter.post('/', loadGenres )
 
