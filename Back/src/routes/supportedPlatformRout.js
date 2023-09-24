ad04ab0dd9782968aae7e2d4a3dfe2404b23ff2c
TwilightMaster23 <briansilva1705@gmail.com>
2023-09-24 17:16:39 -0300

    Se agregan las rutas para traer todas las plataformas y las estadisticas

--- Back/src/routes/supportedPlatformRout.js
+++ Back/src/routes/supportedPlatformRout.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 const { loadSupportedPlatform} = require('../controllers/saveGamesControllers/saveGames');
+const { platformStatistics, getPlatforms } = require('../handlers/platformHandlers');
 const supportedPlatform = Router();
 
-supportedPlatform.post('/', loadSupportedPlatform )
+supportedPlatform.post('/', loadSupportedPlatform );
+supportedPlatform.get('/statistics', platformStatistics);
+supportedPlatform.get('/', getPlatforms);
 
 module.exports = supportedPlatform;
