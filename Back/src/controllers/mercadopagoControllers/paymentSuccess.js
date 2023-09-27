6be40d32ece90076d32a5ec0e5d5a2c3c279cf29
negrura14 <talleresmatador114@gmail.com>
2023-09-26 21:16:28 -0300

    modificacion de la ruta en mercado pago

--- Back/src/controllers/mercadopagoControllers/paymentSuccess.js
+++ Back/src/controllers/mercadopagoControllers/paymentSuccess.js
@@ -23,7 +23,7 @@ const paymentSuccess = async (req, res) => {
 
     console.log('Payment record created:', registroPago.toJSON());
 
-    res.redirect('https://8-bits-front.vercel.app/');
+    res.redirect(('http://localhost:3000/Payment-success/'));
   } catch (error) {
     console.error('Error creating payment record', error);
     return res.status(500).json({ error: error.message });
