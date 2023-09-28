d85bdec91a8f74f19676590f3c26b42b8947c150
TwilightMaster23 <briansilva1705@gmail.com>
2023-09-28 14:18:29 -0300

    Se crea la ruta y el handler para traer los datos de las ventas

--- Back/src/handlers/paymentHandlers.js
+++ Back/src/handlers/paymentHandlers.js
@@ -1,4 +1,4 @@
-const { Payment, Game, User } = require('../db');
+const { Payment, Game, User, conn } = require('../db');
 
 
 const getPayments = async (req, res) => {
@@ -39,4 +39,42 @@ const getPaymentsByGameId = async (req, res)=>{
   }
 }
 
-module.exports = { getPayments, getPaymentsByGameId }
+const getTopSellingGames = async (req, res) => {
+const Sequelize = conn;
+
+    try {
+        const data = await Payment.findAll({
+            attributes: [
+                [Sequelize.fn('COUNT', Sequelize.col('Payment.id')), 'quantitySold'],
+                [Sequelize.col('game.name'), 'gameName'],
+              ],
+              include: [
+                {
+                  model: Game,
+                  attributes: [],
+                  as: 'game',
+                },
+              ],
+              group: ['game.id', 'gameName'],
+              order: [[Sequelize.fn('COUNT', Sequelize.col('Payment.id')), 'DESC']],
+              limit: 5,
+          });
+      
+          res.json(data);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
+const getTotalSales = async (req,res) => {
+    try {
+        const totalGamesSold = await Payment.sum('quentity');
+        const totalRevenue = await Payment.sum('amount');
+
+        res.json({ totalGamesSold, totalRevenue })
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
+module.exports = { getPayments, getPaymentsByGameId, getTopSellingGames, getTotalSales }
--- Back/src/routes/paymentRout.js
+++ Back/src/routes/paymentRout.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
-const { getPayments,getPaymentsByGameId } = require('../handlers/paymentHandlers');
+const { getPayments,getPaymentsByGameId, getTopSellingGames, getTotalSales } = require('../handlers/paymentHandlers');
 
 const paymentRouter = Router();
 
 paymentRouter.get('/', getPayments);
-paymentRouter.get('/:id', getPaymentsByGameId)
+paymentRouter.get('/topselling', getTopSellingGames);
+paymentRouter.get('/totalsales', getTotalSales);
+paymentRouter.get('/:id', getPaymentsByGameId);
 
 module.exports = paymentRouter;
