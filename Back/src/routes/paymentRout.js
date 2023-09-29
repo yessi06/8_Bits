597d81b984421fb69d647b641effba4b4baecf59
SteveFaj <stevefajardo@gmail.com>
2023-09-29 20:43:16 +0100

    modificacion de ruta payment

--- Back/src/routes/paymentRout.js
+++ Back/src/routes/paymentRout.js
@@ -5,7 +5,7 @@ const paymentRouter = Router();
 paymentRouter.get('/', getPayments);
 paymentRouter.get('/topselling', getTopSellingGames);
 paymentRouter.get('/totalsales', getTotalSales);
-paymentRouter.get('/:id', getPaymentsByGameId);
+paymentRouter.get('/game/:id', getPaymentsByGameId);
 paymentRouter.get('/user/:id', getPaymentsByUserId);
 
 
