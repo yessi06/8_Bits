4390650d9098b1cbf01a1e2614bd064df80acbe6
yessi06 <yessicac187@gmail.com>
2023-09-28 22:07:43 -0500

    Se modifica logica para guardar el pago en la base de datos Payment

--- Back/src/controllers/mercadopagoControllers/paymentSuccess.js
+++ Back/src/controllers/mercadopagoControllers/paymentSuccess.js
@@ -7,23 +7,25 @@ const paymentSuccess = async (req, res) => {
     const idPayment = datosPago.payment_id;
     const status = datosPago.status;
     const amount = req.query.unitPrice;
-    const quentity = req.query.shoppingQuantity;
+    const quantity = req.query.shoppingQuantity;
     const idUser = req.query.idUser;
     const idGame = req.query.idGame;
 
+    console.log('idPayment:', idPayment);
+    console.log('status:', status);
 
     const registroPago = await Payment.create({
       idPayment,
       status,
       amount,
-      quentity,
+      quantity,
       idUser,
       idGame,
     });
 
     console.log('Payment record created:', registroPago.toJSON());
 
-    res.redirect(('https://8-bits-front.vercel.app/home'));
+    res.redirect(`https://8-bits-front.vercel.app/payment-success`);
   } catch (error) {
     console.error('Error creating payment record', error);
     return res.status(500).json({ error: error.message });
